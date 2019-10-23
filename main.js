edfefb3b9028b995eda3007db20f406d1b5825c1
Anathi Anathi <anathi.gumede@codeimmersives.com>
2019-10-23 15:45:29 -0400

    repeat possible solution

--- main.js
+++ main.js
@@ -1,8 +1,28 @@
 function slice(string, start = 0, end = string.length) {
+  const newStr = '';
+  //const string = str[i];
+  for (let i = start; i < end; i++) {
+    //slice.push()
+  }
+  return newStr;
 }
 
 function repeat(str, repetitions) {
+  if(repetitions < 0) 
+    return "";
+  if(repetitions === 1) 
+    return str;
+  else 
+    return str + repeat(str, repetitions - 1);
+  //const newStr = '';
+  //for (let i = 0; i < str.length; i++)
+    //if (repetitions > 0) {
+      //return (str * repetitions)
+    //}
+  
+  //return newStr;
 }
+
 
 function startsWith(string, substring) {
 }
